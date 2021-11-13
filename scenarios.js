d4f323bf6a97fb1ee6f189b33590538977a490d3
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-13 12:23:20 -0800

    added first scenario

---
+++ scenarios.js
@@ -0,0 +1,11 @@
+const scenarios = [
+	{
+		name: "falling-ball",
+		load: () => {
+			app.physicsWorld = new PhysicsWorld([
+				new PhysicsObject(new Circle(new Vector(app.canvasIO.canvas.width / 2, app.canvasIO.canvas.height / 2), 50))
+			]);
+		}
+	}
+];
+scenarios[0].load();
