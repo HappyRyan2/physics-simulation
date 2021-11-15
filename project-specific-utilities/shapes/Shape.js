5382a186051a3267d8ca9849e029524ac5b93cca
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-15 13:02:54 -0800

    added method Shape.circleIntersectsLine

--- project-specific-utilities/shapes/Shape.js
+++ project-specific-utilities/shapes/Shape.js
@@ -3,6 +3,15 @@ class Shape {
 		const distance = circle1.position.subtract(circle2.position).magnitude;
 		return (distance <= circle1.radius + circle2.radius);
 	}
+	static circleIntersectsLine(circle, line) {
+		if(line.endpoint1.x === line.endpoint2.x) {
+			return Math.dist(line.endpoint1.x, circle.endpoint1.x) <= circle.radius;
+		}
+		const slope = line.slope();
+		const intercept = line.yIntercept();
+		const radius = circle.radius;
+		return ((2 * slope * intercept) ** 2 - 4 * (slope ** 2 + 1) * (intercept ** 2 - radius ** 2)) >= 0;
+	}
 }
 
 
@@ -18,5 +27,19 @@ testing.addUnit("Shape.circleIntersectsCircle()", {
 		const circle2 = new Circle(20, 15, 4);
 		const intersect = Shape.circleIntersectsCircle(circle1, circle2);
 		expect(intersect).toEqual(false);
+	}
+});
+testing.addUnit("Shape.circleIntersectsLine()", {
+	"returns true when the circle and the line intersect": () => {
+		const circle = new Circle(0, 0, 5);
+		const line = new Line(0, 1, 1, 2);
+		const intersects = Shape.circleIntersectsLine(circle, line);
+		expect(intersects).toEqual(true);
+	},
+	"returns false when the circle and the line do not intersect": () => {
+		const circle = new Circle(0, 0, 5);
+		const line = new Line(0, 10, 1, 11);
+		const intersects = Shape.circleIntersectsLine(circle, line);
+		expect(intersects).toEqual(false);
 	}
 });
