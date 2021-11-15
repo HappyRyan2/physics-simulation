c18e1d23355f0635c22dc3f46f8afa40a3c1cf61
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-15 09:15:57 -0800

    improved constructors + tests for Polygons and Circles

--- project-specific-utilities/shapes/Circle.js
+++ project-specific-utilities/shapes/Circle.js
@@ -1,8 +1,16 @@
 class Circle extends Shape {
-	constructor(position, radius) {
+	constructor() {
 		super();
-		this.position = position ?? new Vector(0, 0);
-		this.radius = radius ?? 1;
+		if([...arguments].every(v => typeof v === "number")) {
+			const [x, y, r] = arguments;
+			this.position = new Vector(x, y);
+			this.radius = r;
+		}
+		else if(arguments[0] instanceof Vector && typeof arguments[1] === "number") {
+			const [position, radius] = arguments;
+			this.position = position ?? new Vector(0, 0);
+			this.radius = radius ?? 1;
+		}
 	}
 
 	display(c) {
@@ -11,3 +19,16 @@ class Circle extends Shape {
 		c.fill();
 	}
 }
+
+testing.addUnit("Circle constructor", {
+	"can create a Circle from an x-position, a y-position, and a radius": () => {
+		const circle = new Circle(1, 2, 3);
+		expect(circle.position).toEqual(new Vector(1, 2));
+		expect(circle.radius).toEqual(3);
+	},
+	"can create a Circle from a position and a radius": () => {
+		const circle = new Circle(new Vector(1, 2), 3);
+		expect(circle.position).toEqual(new Vector(1, 2));
+		expect(circle.radius).toEqual(3);
+	}
+});
--- project-specific-utilities/shapes/Polygon.js
+++ project-specific-utilities/shapes/Polygon.js
@@ -1,7 +1,18 @@
 class Polygon extends Shape {
-	constructor(vertices) {
+	constructor() {
 		super();
-		this.vertices = vertices;
+		if([...arguments].every(v => typeof v === "number")) {
+			this.vertices = [];
+			for(let i = 0; i < arguments.length; i += 2) {
+				this.vertices[i / 2] = new Vector(arguments[i], arguments[i + 1]);
+			}
+		}
+		else if(arguments[0].every(v => v instanceof Vector)) {
+			[this.vertices] = arguments;
+		}
+		else {
+			throw new Error("Unsupported input format.");
+		}
 	}
 
 	display(c) {
@@ -13,3 +24,23 @@ class Polygon extends Shape {
 		c.fill();
 	}
 }
+
+testing.addUnit("Polygon constructor", {
+	"can create a Polygon from an array of Vectors": () => {
+		const vertices = [
+			new Vector(0, 0),
+			new Vector(1, 0),
+			new Vector(0, 1)
+		];
+		const polygon = new Polygon(vertices);
+		expect(polygon.vertices).toEqual(vertices);
+	},
+	"can create a Polygon from the x and y values of the vertices": () => {
+		const polygon = new Polygon(1, 2, 3, 4, 5, 6);
+		expect(polygon.vertices).toEqual([
+			new Vector(1, 2),
+			new Vector(3, 4),
+			new Vector(5, 6)
+		]);
+	}
+});
