fa30a47092bb13f43d7b4041c8de83ba9f47908c
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-15 14:04:52 -0800

    added method Shape.circleIntersectsSegment

--- project-specific-utilities/shapes/Shape.js
+++ project-specific-utilities/shapes/Shape.js
@@ -12,6 +12,40 @@ class Shape {
 		const radius = circle.radius;
 		return ((2 * slope * intercept) ** 2 - 4 * (slope ** 2 + 1) * (intercept ** 2 - radius ** 2)) >= 0;
 	}
+	static circleIntersectsSegment(circle, segment) {
+		if(segment.endpoint1.x === segment.endpoint2.x) {
+			if(Math.dist(segment.endpoint1.x, circle.position.x) > circle.radius) {
+				return false;
+			}
+			const x = segment.endpoint1.x;
+			const y1 = segment.endpoint1.y;
+			const y2 = segment.endpoint2.y;
+			const circleY1 = circle.position.y + Math.sqrt(circle.radius ** 2 - x ** 2);
+			const circleY2 = circle.position.y - Math.sqrt(circle.radius ** 2 - x ** 2);
+			return !(
+				(y1 > circleY1 && y2 > circleY1) ||
+				(y1 < circleY2 && y2 < circleY2)
+			);
+		}
+		const radius = circle.radius;
+		if(segment.endpoint1.subtract(circle.position).magnitude <= radius) {
+			return true;
+		}
+		const slope = new Line(segment).slope();
+		const intercept = new Line(segment).yIntercept();
+		const discriminant = (2 * slope * intercept) ** 2 - 4 * (slope ** 2 + 1) * (intercept ** 2 - radius ** 2);
+		if(discriminant < 0) {
+			return false;
+		}
+		const xIntersection1 = ((-2 * slope * intercept) + Math.sqrt(discriminant)) / (2 * (slope ** 2 + 1));
+		const xIntersection2 = ((-2 * slope * intercept) - Math.sqrt(discriminant)) / (2 * (slope ** 2 + 1));
+		const minX = Math.min(segment.endpoint1.x, segment.endpoint2.x);
+		const maxX = Math.max(segment.endpoint1.x, segment.endpoint2.x);
+		return (
+			(minX <= xIntersection1 && xIntersection1 <= maxX) ||
+			(minX <= xIntersection2 && xIntersection2 <= maxX)
+		);
+	}
 }
 
 
@@ -41,5 +75,56 @@ testing.addUnit("Shape.circleIntersectsLine()", {
 		const line = new Line(0, 10, 1, 11);
 		const intersects = Shape.circleIntersectsLine(circle, line);
 		expect(intersects).toEqual(false);
+	}
+});
+testing.addUnit("Shape.circleIntersectsSegment()", {
+	"returns true when the circle and the line segment intersect": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(1, 1, 5, 5);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(true);
+	},
+	"returns false when the circle and the segment do not intersect": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(0, 100, 1, 101);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(false);
+	},
+	"returns false when the line intersects the circle but the segment does not": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(100, 100, 110, 110);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(false);
+	},
+	"returns true when the line segment is contained within the circle": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(-1, -1, 1, 1);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(true);
+	},
+
+	"returns true when the circle and the line segment intersect and the line segment is vertical": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(3, -5, 3, 0);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(true);
+	},
+	"returns false when the circle and the line segment do not intersect and the line is vertical": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(10, 0, 10, 1);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(false);
+	},
+	"returns false when the line intersects the circle but the segment does not and the line segment is vertical": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(0, 100, 0, 110);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(false);
+	},
+	"returns true when the line segment is vertical and is contained within the circle": () => {
+		const circle = new Circle(0, 0, 5);
+		const segment = new Segment(0, 0, 0, 1);
+		const intersect = Shape.circleIntersectsSegment(circle, segment);
+		expect(intersect).toEqual(true);
 	}
 });
