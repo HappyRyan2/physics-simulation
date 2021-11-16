c59953f9515d201e805fd0aba7790d594832e5df
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-16 12:45:58 -0800

    added new methods Circle.rotate and Circle.translate

--- project-specific-utilities/shapes/Circle.js
+++ project-specific-utilities/shapes/Circle.js
@@ -13,6 +13,18 @@ class Circle extends Shape {
 		}
 	}
 
+	translate(vector) {
+		if(!(vector instanceof Vector)) {
+			return this.translate(new Vector(...arguments));
+		}
+		return new Circle(this.position.add(vector), this.radius);
+	}
+	rotate(angle) {
+		const result = new Circle(new Vector(this.position), this.radius);
+		result.position.angle += angle;
+		return result;
+	}
+
 	display(c) {
 		c.beginPath();
 		c.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
@@ -30,5 +42,29 @@ testing.addUnit("Circle constructor", {
 		const circle = new Circle(new Vector(1, 2), 3);
 		expect(circle.position).toEqual(new Vector(1, 2));
 		expect(circle.radius).toEqual(3);
+	}
+});
+testing.addUnit("Circle.translate()", {
+	"correctly returns the circle after the translation": () => {
+		const circle = new Circle(1, 2, 100);
+		const translated = circle.translate(3, 4);
+		expect(translated).toEqual(new Circle(1 + 3, 2 + 4, 100));
+	},
+	"does not modify the original Circle": () => {
+		const circle = new Circle(1, 2, 100);
+		const translated = circle.translate(3, 4);
+		expect(circle).toEqual(new Circle(1, 2, 100));
+	}
+});
+testing.addUnit("Circle.rotate()", {
+	"correctly returns the circle after the rotation about the origin": () => {
+		const circle = new Circle(1, 2, 5);
+		const rotated = circle.rotate(90);
+		expect(rotated).toEqual(new Circle(2, -1, 5));
+	},
+	"does not modify the original Circle": () => {
+		const circle = new Circle(1, 2, 5);
+		const rotated = circle.rotate(90);
+		expect(circle).toEqual(new Circle(1, 2, 5));
 	}
 });
