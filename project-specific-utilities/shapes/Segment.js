8a70ea51575301040c23fc19afab211e83c73aa1
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-16 14:12:37 -0800

    added new methods Segment.translate and Segment.rotate

--- project-specific-utilities/shapes/Segment.js
+++ project-specific-utilities/shapes/Segment.js
@@ -19,6 +19,19 @@ class Segment extends Shape {
 	isVertical() {
 		return this.endpoint1.x === this.endpoint2.x;
 	}
+
+	translate(vector) {
+		if(!(vector instanceof Vector)) {
+			return this.translate(new Vector(...arguments));
+		}
+		return new Segment(this.endpoint1.add(vector), this.endpoint2.add(vector));
+	}
+	rotate(angle) {
+		const result = new Segment(new Vector(this.endpoint1), new Vector(this.endpoint2));
+		result.endpoint1.angle += angle;
+		result.endpoint2.angle += angle;
+		return result;
+	}
 }
 
 testing.addUnit("Segment constructor", {
@@ -40,5 +53,32 @@ testing.addUnit("Segment constructor", {
 		const segment = new Segment(line);
 		expect(segment.endpoint1).toEqual(new Vector(1, 2));
 		expect(segment.endpoint2).toEqual(new Vector(3, 4));
+	}
+});
+testing.addUnit("Segment.translate()", {
+	"correctly returns the line segment after the translation": () => {
+		const segment = new Segment(0, 0, 1, 1);
+		const translated = segment.translate(5, 0);
+		expect(translated).toEqual(new Segment(5, 0, 6, 1));
+	},
+	"does not modify the original line segment": () => {
+		const segment = new Segment(0, 0, 1, 1);
+		const translated = segment.translate(5, 0);
+		expect(segment).toEqual(new Segment(0, 0, 1, 1));
+	}
+});
+testing.addUnit("Segment.rotate()", {
+	"correctly returns the line segment after the rotation about the origin": () => {
+		const segment = new Segment(0, 0, 1, -1);
+		const rotated = segment.rotate(90);
+		expect(rotated.endpoint1.x).toApproximatelyEqual(0);
+		expect(rotated.endpoint1.y).toApproximatelyEqual(0);
+		expect(rotated.endpoint2.x).toApproximatelyEqual(-1);
+		expect(rotated.endpoint2.y).toApproximatelyEqual(-1);
+	},
+	"does not modify the original line segment": () => {
+		const segment = new Segment(0, 0, 1, 1);
+		const rotated = segment.rotate(90);
+		expect(segment).toEqual(new Segment(0, 0, 1, 1));
 	}
 });
