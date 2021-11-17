2a86d7247223ccc60682aad3348f9f4cde5494fc
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-17 14:25:01 -0800

    modified positions of objects in scenarios

--- scenarios.js
+++ scenarios.js
@@ -15,15 +15,17 @@ const scenarios = [
 			const { width, height } = app.canvasIO.canvas;
 			app.physicsWorld = new PhysicsWorld([
 				new PhysicsObject({
-					shape: new Circle(new Vector(width / 2, height / 2), 50),
+					shape: new Circle(0, 0, 50),
+					position: new Vector(width / 2, height / 2)
 				}),
 				new PhysicsObject({
 					shape: new Polygon([
-						new Vector(0, height * 7/8),
-						new Vector(width, height * 7/8),
-						new Vector(width, height),
-						new Vector(0, height)
+						new Vector(-width / 2, -height * 1/16),
+						new Vector(width / 2, -height * 1/16),
+						new Vector(width / 2, height * 1/16),
+						new Vector(-width / 2, height * 1/16),
 					]),
+					position: new Vector(width / 2, height * 15/16),
 					antigravity: true
 				})
 			], 0.1);
