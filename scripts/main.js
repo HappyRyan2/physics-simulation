cfde91dfbcafc2b3e9eaa2e22e8fc0bbfdee6d2f
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-25 13:56:25 -0800

    added framerate display

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,13 @@ class App {
 		this.fps = 60;
 		this.frameCount = 0;
 		this.canvasIO = new CanvasIO();
+		this.frameTimes = [];
+		this.actualFPS = 60;
 	}
 	initialize() {
 		this.canvasIO.activate();
 		window.setInterval(() => {
+			this.frameTimes.push(Date.now());
 			this.update();
 			this.display();
 		}, 1000 / this.fps);
@@ -16,11 +19,15 @@ class App {
 	display() {
 		this.physicsWorld.display(this.canvasIO.ctx);
 		this.displayFrameCount(this.canvasIO.ctx);
+		this.displayFrameRate(this.canvasIO.ctx);
 	}
 	update() {
 		if(!this.physicsWorld.paused) {
 			this.physicsWorld.update();
 			this.frameCount ++;
+			if(this.frameCount % 10 === 0) {
+				this.updateFPS();
+			}
 		}
 	}
 
@@ -30,6 +37,19 @@ class App {
 		c.fillStyle = "black";
 		c.font = "20px monospace";
 		c.fillText(this.frameCount, 0, 0);
+	}
+	displayFrameRate(c) {
+		c.textAlign = "right";
+		c.textBaseline = "top";
+		c.fillStyle = "black";
+		c.font = "20px monospace";
+		c.fillText(Math.round(this.actualFPS), c.canvas.width, 0);
+	}
+
+	updateFPS() {
+		const NUM_FRAMES = 10;
+		const duration = this.frameTimes[this.frameTimes.length - 1] - this.frameTimes[this.frameTimes.length - 1 - NUM_FRAMES];
+		this.actualFPS = NUM_FRAMES * 1000 / duration;
 	}
 }
 
